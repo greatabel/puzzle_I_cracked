46148d79fde5679d2fe4131a4f48aa322753ff85
greatabel <myreceiver2for2github@gmail.com>
2019-01-22 18:49:13 +0800

    change iterate into not-iterate

--- ProjectEuler/i76Counting_summations.py
+++ ProjectEuler/i76Counting_summations.py
@@ -11,15 +11,27 @@
 
 import time
 from termcolor import colored
+import math
 
 
-def main_process():
-    print(colored('mycount=', 'red'), 'results')
+def find_allways(total, sizes):
+    L = [0 for i in range(total+1)]
+    L[0] = 1
+    for i in range(len(sizes)):
+        for j in range(sizes[i], len(L)):
+            L[j] += L[j -sizes[i]]
+    print(L[total] )
+
 
 if __name__ == "__main__":
     tic = time.clock()
-    
-    main_process()
+    # for i in range(0,20):
+    #     print(i,fib(i),len(str(fib(i))))
+    # find_allpowers(5,5)
+    # find_allpowers(10000,4)
+
+    find_allways(100, list(range(1, 100)))
+
 
     toc = time.clock()
     print("time=",toc - tic)
