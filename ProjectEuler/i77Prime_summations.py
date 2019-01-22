dfdc4adbea458ac69e268f84eaacff4b4d5b9f25
greatabel <myreceiver2for2github@gmail.com>
2019-01-22 19:20:39 +0800

    solve i77

--- ProjectEuler/i77Prime_summations.py
+++ ProjectEuler/i77Prime_summations.py
@@ -11,15 +11,43 @@ What is the first value which can be written as the sum of primes in over five t
 '''
 import time
 from termcolor import colored
+import math
+from math import gcd
 
 
-def main_process():
-    print(colored('mycount=', 'red'), 'results')
+def find_allways(total, sizes):
+    L = [0 for i in range(total+1)]
+    L[0] = 1
+    for i in range(len(sizes)):
+        for j in range(sizes[i], len(L)):
+            L[j] += L[j -sizes[i]]
+    print(L[total])
+    return L[total]
+
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 if __name__ == "__main__":
     tic = time.clock()
-    
-    main_process()
+    # for i in range(0,20):
+    #     print(i,fib(i),len(str(fib(i))))
+    # find_allpowers(5,5)
+    # find_allpowers(10000,4)
+    for limit in range(1, 1000):
+        
+        primes = []
+        for i in range(2, limit):
+            if is_prime(i):
+                primes.append(i)
+        r = find_allways(limit, primes)
+        if r >= 5000:
+            print('#'*10, limit, r)
+            break
+        
+
 
     toc = time.clock()
     print("time=",toc - tic)
