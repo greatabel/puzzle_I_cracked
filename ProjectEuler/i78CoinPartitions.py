e2f1f8cc2bfbd923914e3bff07a37851d59c96d0
greatabel <myreceiver2for2github@gmail.com>
2019-01-23 10:23:42 +0800

    too slow version

--- ProjectEuler/i78CoinPartitions.py
+++ ProjectEuler/i78CoinPartitions.py
@@ -13,15 +13,33 @@ Find the least value of n for which p(n) is divisible by one million.
 
 import time
 from termcolor import colored
+import math
 
+# 太慢
+# def find_allways(total, sizes):
+#     L = [0 for i in range(total+1)]
+#     L[0] = 1
+#     for i in range(len(sizes)):
+#         for j in range(sizes[i], len(L)):
+#             L[j] += L[j -sizes[i]]
 
-def main_process():
-    print(colored('mycount=', 'red'), 'results')
+#     # print(L[total])
+#     return L[total] + 1
+
 
 if __name__ == "__main__":
     tic = time.clock()
-    
-    main_process()
+    d = 1000000
+    # d = 1000
+    # my_estimate = 2019
+    # for n in range(1, my_estimate):
+    #     p_n = find_allways(n, list(range(1, n+1)))
+    #     if p_n % d == 0:
+    #         print(n, 'p(n)=', p_n)
+    #         break
+    #     if n % 100 == 0:
+    #         print('n=', n)
+
 
     toc = time.clock()
     print("time=",toc - tic)
