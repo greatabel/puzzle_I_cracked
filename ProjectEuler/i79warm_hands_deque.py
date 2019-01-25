5dbeb72f4fcdb1ddec37a7c094b4b03d5f14855f
greatabel <myreceiver2for2github@gmail.com>
2019-01-25 15:46:37 +0800

    i79warm_hands_deque.py demo

--- ProjectEuler/i79warm_hands_deque.py
+++ ProjectEuler/i79warm_hands_deque.py
@@ -5,7 +5,17 @@ def main_process():
     d = deque('ghi')
     for elem in d:
         print(elem)
+    print('d=', d)
+    d.append('j')
+    d.append('k')
+    print('d=', d)
 
-        
+    pop = d.pop()
+    print('pop=', pop)
+    popleft = d.popleft()
+    print('popleft=', popleft)
+    print('d=', d)
+
+
 if __name__ == "__main__":
     main_process()
