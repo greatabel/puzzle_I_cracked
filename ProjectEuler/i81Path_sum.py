1dbb2e9b9d68a18c4909a7b63901149088f85bc5
greatabel <myreceiver2for2github@gmail.com>
2019-01-30 16:39:19 +0800

    solve i81

--- ProjectEuler/i81Path_sum.py
+++ ProjectEuler/i81Path_sum.py
@@ -19,7 +19,20 @@ def main_process():
             row = [int(r) for r in line.split(',')]
             matrix.append(row)
 
-    print(colored('mycount=', 'red'), 'results')
+    for i in range(1, len(matrix[0])):
+        matrix[0][i] += matrix[0][i-1]
+
+    for i in range(1, len(matrix)):
+        matrix[i][0] += matrix[i-1][0]
+
+    for i in range(1, len(matrix)):
+        for j in range(1, len(matrix[1])):
+            matrix[i][j] += min(matrix[i-1][j], matrix[i][j-1])
+
+    D = 80
+
+    print(colored('mycount=', 'red'), matrix)
+    print(matrix[D-1][D-1])
 
 if __name__ == "__main__":
     tic = time.clock()
