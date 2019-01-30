588a4f517fd2b9ad74bc59b8b29efef6b602edfc
greatabel <myreceiver2for2github@gmail.com>
2019-01-30 16:54:12 +0800

    solve i82

--- ProjectEuler/i82Path_sum.py
+++ ProjectEuler/i82Path_sum.py
@@ -11,6 +11,21 @@ a 31K text file containing a 80 by 80 matrix, from the left column to the right 
 import time
 from termcolor import colored
 
+def sum_to(i1, i2, j, matrix):
+    x1 = min(i1, i2)
+    x2 = max(i1, i2)
+    return sum([matrix[x][j] for x in range(x1, x2+1)])
+
+# Returns the minimum path sum that goes from any entry in the                                                                                          
+# leftmost column to entry (i, j).                                                                                                                      
+def compute_min_path_entry(i, j, matrix, min_path):
+    n = len(matrix)
+    # Possible paths: take the minimum cost path to somewhere in the                                                                                    
+    # (j-1)th column, go right, and then go up or down until reaching                                                                                   
+    # (i, j).                                                                                                                                           
+    possibilities = [min_path[x][j-1] + sum_to(x, i, j, matrix)
+                     for x in range(n)]
+    return min(possibilities)
 
 def main_process():
     myfile = open('i82_matrix.txt').read()
@@ -19,7 +34,25 @@ def main_process():
         if line and line != '':
             row = [int(r) for r in line.split(',')]
             matrix.append(row)
-    print(colored('mycount=', 'red'), 'results')
+
+    n = len(matrix) # assumes matrix is square                                                                                                              
+
+    # Initialize min_path.                                                                                                                                  
+    # min_path[x,y] will hold the minimal path sum starting at any entry in the                                                                             
+    # leftmost column and ending at (x,y).                                                                                                                  
+    min_path = [[-1 for y in row] for row in matrix]
+    for i in range(n):
+        min_path[i][0] = matrix[i][0]
+
+    # When min_path has been filled up to the j'th column, we can fill in                                                                                   
+    # the (j+1)st column.                                                                                                                                   
+    for j in range(1, n):
+        for i in range(n):
+            min_path[i][j] = compute_min_path_entry(i, j, matrix, min_path)
+
+    result = min([min_path[i][n-1] for i in range(n)])
+    
+    print(colored('mycount=', 'red'), result)
 
 if __name__ == "__main__":
     tic = time.clock()
