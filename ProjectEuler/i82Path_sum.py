142b7e3c1c91392d15f5491cdf57bed76eb6ebc7
greatabel <myreceiver2for2github@gmail.com>
2019-01-30 16:42:42 +0800

    i82Path_sum.py

---
+++ ProjectEuler/i82Path_sum.py
@@ -0,0 +1,24 @@
+'''
+The minimal path sum in the 5 by 5 matrix below, by starting in any cell 
+in the left column and finishing in any cell in the right column, and only moving up, down, 
+and right, is indicated in red and bold; the sum is equal to 994.
+
+⎛⎝⎜⎜⎜⎜⎜⎜131201630537805673968036997322343427464975241039654221213718150111956331⎞⎠⎟⎟⎟⎟⎟⎟
+Find the minimal path sum, in matrix.txt (right click and "Save Link/Target As..."), 
+a 31K text file containing a 80 by 80 matrix, from the left column to the right column.
+'''
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.clock()
+    
+    main_process()
+
+    toc = time.clock()
+    print("time=",toc - tic)
