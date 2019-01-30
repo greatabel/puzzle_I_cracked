2256480e8d3e34ed7f0c71618d2864aad7ee0250
greatabel <myreceiver2for2github@gmail.com>
2019-01-30 15:33:18 +0800

    i81Path_sum.py

---
+++ ProjectEuler/i81Path_sum.py
@@ -0,0 +1,23 @@
+'''
+In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right,
+ by only moving to the right and down, is indicated in bold red and is equal to 2427.
+
+⎛⎝⎜⎜⎜⎜⎜⎜131 201630537805673968036997322343427464975241039654221213718150111956331⎞⎠⎟⎟⎟⎟⎟⎟
+Find the minimal path sum, in matrix.txt (right click and "Save Link/Target As..."), 
+a 31K text file containing a 80 by 80 matrix, from the top left to the bottom right by only moving right and down.
+'''
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.clock()
+    
+    main_process()
+
+    toc = time.clock()
+    print("time=",toc - tic)
