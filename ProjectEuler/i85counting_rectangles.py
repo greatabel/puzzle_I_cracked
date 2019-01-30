8ccd7f0c3418c63d230c1aa83ab52af71e604f8b
greatabel <myreceiver2for2github@gmail.com>
2019-01-30 17:23:15 +0800

    i85 desc problem

--- ProjectEuler/i85counting_rectangles.py
+++ ProjectEuler/i85counting_rectangles.py
@@ -1,14 +1,11 @@
+'''
+By counting carefully it can be seen 
+that a rectangular grid measuring 3 by 2 contains eighteen rectangles:
 
 
-
-
-
-
-
-
-
-
-
+Although there exists no rectangular grid that contains 
+exactly two million rectangles, find the area of the grid with the nearest solution.
+'''
 
 import time
 from termcolor import colored
