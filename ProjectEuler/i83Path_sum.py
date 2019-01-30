f61fa200dea193cf0224d8f6bd2173d7a8a50c38
greatabel <myreceiver2for2github@gmail.com>
2019-01-30 16:56:06 +0800

    i83 desc

--- ProjectEuler/i83Path_sum.py
+++ ProjectEuler/i83Path_sum.py
@@ -1,14 +1,12 @@
+'''
+In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, by moving left, right, up, and down, 
+is indicated in bold red and is equal to 2297.
 
-
-
-
-
-
-
-
-
-
-
+⎛⎝⎜⎜⎜⎜⎜⎜131201630537805673968036997322343427464975241039654221213718150111956331⎞⎠⎟⎟⎟⎟⎟⎟
+Find the minimal path sum, in matrix.txt (right click and "Save Link/Target As..."),
+ a 31K text file containing a 80 by 80 matrix, 
+ from the top left to the bottom right by moving left, right, up, and down.
+'''
 
 import time
 from termcolor import colored
