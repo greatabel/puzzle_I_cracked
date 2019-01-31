76f310a8cb98b9f18c3916482a610dcc8b70a5b0
greatabel <myreceiver2for2github@gmail.com>
2019-01-31 21:06:54 +0800

    i85counting_rectangles.py solved

--- ProjectEuler/i85counting_rectangles.py
+++ ProjectEuler/i85counting_rectangles.py
@@ -9,10 +9,23 @@ exactly two million rectangles, find the area of the grid with the nearest solut
 
 import time
 from termcolor import colored
+from math import sqrt
+
 
 
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    L, x, min_diff = 2000000, 2, float('Inf')
+    y = L // 6
+     
+    while x <= y:
+        diff = abs(x*(x + 1) * y*(y + 1) // 4 - L)
+        if diff < min_diff: 
+              area, min_diff, xx, yy = x * y, diff, x, y
+        x += 2
+        y = int(sqrt(L*4 / (x*x + x)))
+     
+    print("Area of grid:", xx, 'x', yy, '=', colored(area, 'red'))
+
 
 if __name__ == "__main__":
     tic = time.clock()
