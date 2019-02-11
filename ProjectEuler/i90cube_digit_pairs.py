4bc3f4658a450f8c1576da252f0be299d9ae2e59
greatabel <myreceiver2for2github@gmail.com>
2019-02-11 17:52:34 +0800

    solved i90

--- ProjectEuler/i90cube_digit_pairs.py
+++ ProjectEuler/i90cube_digit_pairs.py
@@ -31,10 +31,19 @@ import time
 from termcolor import colored
 from itertools import combinations
 
+
+squares = [(0,1), (0,4), (0,6), (1,6), (2,5), (3,6), (4,6), (8,1)]
+
+def valid(c1, c2):
+    return all(x in c1 and y in c2 or x in c2 and y in c1 for x, y in squares)
+
 def main_process():
-    cube = list(combinations(list(range(0,10)), 2))
-    print(cube)
-    print(colored('mycount=', 'red'), 'results')
+    print(list(range(0,8)) + [6])
+    cube = list(combinations([0,1,2,3,4,5,6,7,8,6], 6))
+    isum = sum(1 for i,c1 in enumerate(cube)
+                     for c2 in cube[:i]
+                         if valid(c1, c2))
+    print(colored('mycount=', 'red'), isum)
 
 if __name__ == "__main__":
     tic = time.clock()
