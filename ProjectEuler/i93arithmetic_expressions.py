2f3451c71ded777af70e44ece2bfe0ea9c492af5
greatabel <myreceiver2for2github@gmail.com>
2019-02-18 11:08:55 +0800

    permuations

--- ProjectEuler/i93arithmetic_expressions.py
+++ ProjectEuler/i93arithmetic_expressions.py
@@ -49,12 +49,14 @@ def mytry(a, b, c, d):
     "Helloh"
 
 def main_process():
-    count = 0
+    # count = 0
     for terms in itertools.combinations(range(1, 10), 4):
-        count += 1
-        print(terms)
-    print('count=', count)
-    mytry(1, 2, 3, 4)
+        # count += 1
+        print('terms=', terms)
+        for n in itertools.permutations(terms):
+            print(n)
+    # print('count=', count)
+    # mytry(1, 2, 3, 4)
     print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
