a46aa1a6e963fdd1b517ef381e2c9476e7d057f4
greatabel <myreceiver2for2github@gmail.com>
2019-02-19 15:39:34 +0800

    i94almost_equilateral_triangles.py

--- ProjectEuler/i94almost_equilateral_triangles.py
+++ ProjectEuler/i94almost_equilateral_triangles.py
@@ -12,9 +12,19 @@ and area and whose perimeters do not exceed one billion (1,000,000,000).
 import time
 from termcolor import colored
 
-
+# https://blog.dreamshire.com/project-euler-94-solution/
+limit = 10 ** 9
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    side0, side, s, p, m = 1, 1, 0, 0, 1
+
+    L = 10**9
+    while p < L:
+        side0, side, m = side, 4*side - side0 + 2*m, -m
+        s += p
+        p = 3*side - m
+
+    print("Sum of perimeters less than", L, " =", s)
+    # print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
     tic = time.clock()
