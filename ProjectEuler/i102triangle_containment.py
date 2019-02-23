d50c18693c451be6f054c7b7a6b8f9a70609507b
greatabel <myreceiver2for2github@gmail.com>
2019-02-23 22:10:07 +0800

    solve i102

--- ProjectEuler/i102triangle_containment.py
+++ ProjectEuler/i102triangle_containment.py
@@ -19,10 +19,46 @@ NOTE: The first two examples in the file represent the triangles in the example 
 
 import time
 from termcolor import colored
+import re 
+import math
+import numpy as np
 
+
+
+def isContainmentTraingle(triangle):
+    X1 = triangle[0] - triangle[2]
+    Y1 = triangle[1] - triangle[3]
+    X2 = triangle[4] - triangle[2]
+    Y2 = triangle[5] - triangle[3]
+    S = area(X1,Y1,X2,Y2)
+    for i in range(0,4,2):
+        for j in range(i+2,5,2):
+            S -= area(triangle[i],triangle[i+1],triangle[j],triangle[j+1])
+    return S == 0
+    
+def area(x1,y1,x2,y2):
+    S = np.abs(x1*y2 - x2*y1)
+    return S
+    
+def readData(filename):
+    mat = list()
+    file = open(filename)
+    for line in file:
+        row = line.split(',')
+        row = [int(x) for x in row]
+        mat.append(row)
+    return mat 
+        
 
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    filename = 'i102triangles.txt'
+    mat = readData(filename)
+    mat = np.array(mat)
+    count = 0
+    for line in mat:
+        if isContainmentTraingle(line):
+            count+=1
+    print(colored('mycount=', 'red'), count)
 
 if __name__ == "__main__":
     tic = time.clock()
