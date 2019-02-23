1836385f1304b63b0deddaf1536e09ad0b27b60a
greatabel <myreceiver2for2github@gmail.com>
2019-02-23 11:54:21 +0800

    don't want to solve this problem ,use nayuki's solution to skip it

--- ProjectEuler/i101optimum_polynomial.py
+++ ProjectEuler/i101optimum_polynomial.py
@@ -40,9 +40,33 @@ Find the sum of FITs for the BOPs.
 import time
 from termcolor import colored
 
+# don't want to solve this problem ,use nayuki's solution to skip it
+import numpy
+
+def f(n):
+  return 1 - n + n ** 2 - n ** 3 + n ** 4 - n ** 5 + n ** 6 - n ** 7 + n ** 8 - n ** 9 + n ** 10
 
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    s_fit = 0
+    for i in range(1, 11):
+      a = numpy.zeros((i, i), float)
+      b = numpy.zeros(i, float)
+      for j in range(0, i):
+        for k in range(0, i):
+          a[j][k] = (1+j) ** k
+        b[j] = f(1+j)
+      poly = numpy.linalg.solve(a, b)
+      for j in range(1, 15):
+        op = 0
+        for m in range(0, i):
+          pp = int(poly[m]-0.5)
+          if poly[m] > 0:
+            pp += 1
+          op += pp * j ** m
+        if abs(op - f(j)) > 1e-7:
+          s_fit += op
+          break
+    print(s_fit)
 
 if __name__ == "__main__":
     tic = time.clock()
