54b4d083a3f0e1a19639e0c0983183cfadf5d211
greatabel <myreceiver2for2github@gmail.com>
2019-03-07 17:37:29 +0800

    use another way to solve it

--- 3ProjectEuler/i113non-bouncy_numbers.py
+++ 3ProjectEuler/i113non-bouncy_numbers.py
@@ -20,16 +20,9 @@ import time
 from termcolor import colored
 
 
-def generate_increase(n):
-    nstr = ''
-    if n < 10:
-        for i in range(1, n+1):
-            nstr += str(i)
-    print(nstr)
-
 
 def main_process():
-    generate_increase(9)
+
     print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
