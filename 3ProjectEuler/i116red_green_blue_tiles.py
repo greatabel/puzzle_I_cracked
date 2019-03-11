f3277cfa5bb5b039e47d23ef5d191ff46bbfa841
greatabel <myreceiver2for2github@gmail.com>
2019-03-11 16:55:56 +0800

    desc of i116

--- 3ProjectEuler/i116red_green_blue_tiles.py
+++ 3ProjectEuler/i116red_green_blue_tiles.py
@@ -1,14 +1,24 @@
+'''
+A row of five grey square tiles is to have a number of its tiles replaced with 
+coloured oblong tiles chosen from red (length two), green (length three), or blue (length four).
 
+If red tiles are chosen there are exactly seven ways this can be done.
 
+png116_1.png
+If green tiles are chosen there are three ways.
 
+png116_2.png
+And if blue tiles are chosen there are two ways.
 
+png116_3.png
+Assuming that colours cannot be mixed there are 7 + 3 + 2 = 12 ways of replacing 
+the grey tiles in a row measuring five units in length.
 
+How many different ways can the grey tiles in a row measuring fifty units 
+in length be replaced if colours cannot be mixed and at least one coloured tile must be used?
 
-
-
-
-
-
+NOTE: This is related to Problem 117.
+'''
 
 import time
 from termcolor import colored
