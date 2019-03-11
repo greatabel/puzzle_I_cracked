f065ec410dc79f0c316ce85d69e4cdffdc8f0a98
greatabel <myreceiver2for2github@gmail.com>
2019-03-11 22:16:22 +0800

    desc i117

--- 3ProjectEuler/i117red_green_blue_tiles.py
+++ 3ProjectEuler/i117red_green_blue_tiles.py
@@ -1,13 +1,12 @@
+'''
+Using a combination of grey square tiles and oblong tiles chosen from: 
+red tiles (measuring two units), green tiles (measuring three units), 
+and blue tiles (measuring four units), it is possible to tile a row measuring 
+five units in length in exactly fifteen different ways.
 
-
-
-
-
-
-
-
-
-
+png117.png
+How many ways can a row measuring fifty units in length be tiled?
+'''
 
 
 import time
