69a30219dd9f1a4310319b079eed40fc6f8fe2e8
greatabel <myreceiver2for2github@gmail.com>
2019-03-11 17:47:46 +0800

    fix bug of cache

--- 3ProjectEuler/i116red_green_blue_tiles.py
+++ 3ProjectEuler/i116red_green_blue_tiles.py
@@ -25,43 +25,50 @@ from termcolor import colored
 
 
 len_count_dic = dict()
+len_count_dic.clear()
+
+
 def f(x, y):
 
+
+
+
+    # 一行有x个单元， 我们用y个连续红色单元填充到里面， x >= y
+    # 第一种就是没有用红色划分，全黑的情况
+    ways_count = 0
+
+    # 当红色单元以及超过需要填充的行单元数，应该终止程序
+    if x < y:
+        return ways_count
     #添加缓存逻辑
     if x in len_count_dic and len_count_dic[x]!= 0:
         return len_count_dic[x]
 
-
-    ways_count = 0
-
-    if x < y:
-        return ways_count
-
     for start_index in range(0, x-y+1):
         # print(colored("开始的index=", "red"), start_index)
-        
-        # 可以填充1个
-        ways_count += 1
 
+        ways_count += 1
         ways_count += f(x-start_index-y, y)
 
     # 添加缓存
     len_count_dic[x] = ways_count
+    print(x, y, ways_count)
     return ways_count
 
 
 def main_process():
-    
+    mysum = 0
     x = 50
-    mycount = 0
+    # 测试用小数据
+    # x = 7
     # 直接先不停测试上限，先试验了100不行，再200就可以了
-    for y in range(2, 5):
-        print('y=', y)
-        mycount += f(x, y)
-
-    print(colored('mycount=', 'red'), mycount)
-      
-    # mycount= 1053389 x= 168
+    for yi in range(2 , 5):
+        len_count_dic.clear()
+        mycount = f(x, yi)
+        mysum += mycount
+    # right  = 20492570929
+    # mysum=   61095033219
+    print('mysum=', mysum)
 
 if __name__ == "__main__":
     tic = time.clock()
