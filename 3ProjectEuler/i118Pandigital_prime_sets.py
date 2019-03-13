ffdbd273d2132c05eb69e330d3afc4346483aeb7
greatabel <myreceiver2for2github@gmail.com>
2019-03-13 10:55:49 +0800

    translate problem

--- 3ProjectEuler/i118Pandigital_prime_sets.py
+++ 3ProjectEuler/i118Pandigital_prime_sets.py
@@ -4,6 +4,13 @@ different sets can be formed. Interestingly with the set {2,5,47,89,631},
 all of the elements belonging to it are prime.
 
 How many distinct sets containing each of the digits one through nine exactly once contain only prime elements?
+
+翻译：
+用1～9个连续数字自由连接组成数字，
+可以组成不同的集合。
+有趣的是像 {2,5,47,89,631} 这样的集合，每一个元素都是质数
+
+有多少个这样刚好9个数字 组成的元素集合中每一个都是质数？
 '''
 
 import time
