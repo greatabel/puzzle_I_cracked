8a9fa4f2ca67e6adda26c4fc5df1166cb7b93c1b
greatabel <myreceiver2for2github@gmail.com>
2019-03-13 15:17:01 +0800

    Thought

--- 3ProjectEuler/i118Pandigital_prime_sets.py
+++ 3ProjectEuler/i118Pandigital_prime_sets.py
@@ -5,12 +5,21 @@ all of the elements belonging to it are prime.
 
 How many distinct sets containing each of the digits one through nine exactly once contain only prime elements?
 
-翻译：
+翻译：欧拉工程118
 用1～9个连续数字自由连接组成数字，
 可以组成不同的集合。
 有趣的是像 {2,5,47,89,631} 这样的集合，每一个元素都是质数
 
 有多少个这样刚好9个数字 组成的元素集合中每一个都是质数？
+'''
+
+'''
+思路：
+对于 123456789 进行全排列 A
+
+遍历 A 中每一种情况，对于 a ∈ A， 产生所有关于a的划分
+
+
 '''
 
 import time
