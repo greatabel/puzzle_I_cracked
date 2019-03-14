48b49a840098fcdf48f9883e61efd7a79266f7e2
greatabel <myreceiver2for2github@gmail.com>
2019-03-14 13:53:07 +0800

    sum digits of integer

--- 3ProjectEuler/i119digit_power_sum.py
+++ 3ProjectEuler/i119digit_power_sum.py
@@ -1,8 +1,8 @@
 '''
 The number 512 is interesting because it is equal to the sum of its digits 
-raised to some power: 5 + 1 + 2 = 8, and 83 = 512. 
+raised to some power: 5 + 1 + 2 = 8, and 8^3 = 512. 
 
-Another example of a number with this property is 614656 = 284.
+Another example of a number with this property is 614656 = 28^4.
 
 We shall define an to be the nth term of this sequence and 
 
@@ -17,7 +17,14 @@ import time
 from termcolor import colored
 
 
+def check(n):
+    x = sum(int(digit) for digit in str(n))
+    for i in range(0, 5):
+        print(i, x ** i)
+    
+
 def main_process():
+    check(512)
     print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
