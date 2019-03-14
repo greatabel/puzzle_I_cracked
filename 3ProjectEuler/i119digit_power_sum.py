d9ae43b15f9ec2993c3ac7b53b588401ab719e49
greatabel <myreceiver2for2github@gmail.com>
2019-03-14 14:50:35 +0800

    change way to approach

--- 3ProjectEuler/i119digit_power_sum.py
+++ 3ProjectEuler/i119digit_power_sum.py
@@ -13,6 +13,14 @@ You are given that a2 = 512 and a10 = 614656.
 Find a30.
 '''
 
+'''
+欧拉工程119
+我先试验了正常思路：遍历方法，到12个数开始就非常的慢，所以放弃。
+
+可以考虑到我们要找的30数 肯定是非常大的数，所以我们不能遍历数，而应该考虑遍历次方。
+
+'''
+
 import math
 import time
 from termcolor import colored
@@ -20,33 +28,34 @@ from termcolor import colored
 
 def digit_sum(n):
     num_str = str(n)
-    sum = 0
+    isum = 0
     for i in range(0, len(num_str)):
-        sum += int(num_str[i])
-    return sum
+        isum += int(num_str[i])
+    return isum
 
 
-def interesting(n):
-    x = digit_sum(n)
-    if x < 2:
-        return False
-    y = math.log(n, x)
-    decimal_y = y % 1
-    # python log bug
-    # print(' ', n, 'decimal_y=', decimal_y)
-    if decimal_y < 0.0000001 or decimal_y > 0.9999999:
-        return True
+# def interesting(n):
+#     x = digit_sum(n)
+#     if x < 2:
+#         return False
+#     y = math.log(n, x)
+#     decimal_y = y % 1
+#     # python log bug
+#     # print(' ', n, 'decimal_y=', decimal_y)
+#     if decimal_y < 0.0000001 or decimal_y > 0.9999999:
+#         return True
     
 
 def main_process():
 
-    mycount = 0
-    i = 50
-    while mycount < 30:
-        i += 1
-        if interesting(i):
-            mycount += 1
-            print('mycount:',mycount, i)
+    # mycount = 0
+    # i = 50
+    # while mycount < 30:
+    #     i += 1
+    #     if interesting(i):
+    #         mycount += 1
+    #         print('mycount:',mycount, i)
+
 
 
     print(colored('mycount=', 'red'), 'results')
