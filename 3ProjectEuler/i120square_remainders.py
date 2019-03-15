8cf825f970b41ae4d1026f784e6f3f75ce620a19
greatabel <myreceiver2for2github@gmail.com>
2019-03-15 10:24:50 +0800

    desc i120

--- 3ProjectEuler/i120square_remainders.py
+++ 3ProjectEuler/i120square_remainders.py
@@ -1,14 +1,11 @@
+'''
+Let r be the remainder when (a−1)n + (a+1)n is divided by a^2.
 
+For example, if a = 7 and n = 3, then r = 42: 6^3 + 8^3 = 728 ≡ 42 mod 49. 
+And as n varies, so too will r, but for a = 7 it turns out that rmax = 42.
 
-
-
-
-
-
-
-
-
-
+For 3 ≤ a ≤ 1000, find ∑ rmax.
+'''
 
 import time
 from termcolor import colored
