289ab89a689ba14a13bc7927b45ca19575a0e427
greatabel <myreceiver2for2github@gmail.com>
2019-03-17 08:56:26 +0800

    with desc of i122

--- 3ProjectEuler/i122efficient_exponentiation.py
+++ 3ProjectEuler/i122efficient_exponentiation.py
@@ -1,14 +1,29 @@
+'''
+The most naive way of computing ^15 requires fourteen multiplications:
 
+n × n × ... × n = n^15
 
+But using a "binary" method you can compute it in six multiplications:
 
+n × n = n^2
+n2 × n2 = n^4
+n4 × n4 = n^8
+n8 × n4 = n^12
+n12 × n2 = n^14
+n14 × n = n^15
 
+However it is yet possible to compute it in only five multiplications:
 
+n × n = n^2
+n2 × n = n^3
+n3 × n3 = n^6
+n6 × n6 = n^12
+n12 × n3 = n^15
 
+We shall define m(k) to be the minimum number of multiplications to compute nk; for example m(15) = 5.
 
-
-
-
-
+For 1 ≤ k ≤ 200, find ∑ m(k).
+'''
 
 import time
 from termcolor import colored
