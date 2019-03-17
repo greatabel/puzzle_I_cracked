a32e161bb40a7f909e0f3154a781ebfbf39dae64
greatabel <myreceiver2for2github@gmail.com>
2019-03-17 09:02:49 +0800

    translation

--- 3ProjectEuler/i122efficient_exponentiation.py
+++ 3ProjectEuler/i122efficient_exponentiation.py
@@ -23,6 +23,36 @@ n12 × n3 = n^15
 We shall define m(k) to be the minimum number of multiplications to compute nk; for example m(15) = 5.
 
 For 1 ≤ k ≤ 200, find ∑ m(k).
+
+
+
+
+#----------------------------#
+翻译：
+欧拉工程/欧拉项目 122: 高效指数计算
+
+计算n15最朴素的方式需要14次乘法：
+n × n × … × n = n^15
+
+但使用一个“二进制”的算法，你可以只用6次乘法：
+n × n = n^2
+n2 × n2 = n^4
+n4 × n4 = n^8
+n8 × n4 = n^12
+n12 × n2 = n^14
+n14 × n = n^15
+
+然而，实际上仅用5次乘法也是可以的：
+
+n × n = n^2
+n2 × n = n^3
+n3 × n3 = n^6
+n6 × n6 = n^12
+n12 × n3 = n^15
+
+记m(k)是计算nk所需要的最少次数，例如m(15) = 5。
+
+对于1 ≤ k ≤ 200，求∑m(k)。
 '''
 
 import time
@@ -39,3 +69,8 @@ if __name__ == "__main__":
 
     toc = time.clock()
     print("time=",toc - tic)
+
+
+
+
+
