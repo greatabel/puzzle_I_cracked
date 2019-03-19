ce61898d3cce2cf4da46e6adcbdc3d8cd04ac18e
greatabel <myreceiver2for2github@gmail.com>
2019-03-19 20:36:03 +0800

    desc of i123

--- 3ProjectEuler/i123prime_square_remainders.py
+++ 3ProjectEuler/i123prime_square_remainders.py
@@ -1,13 +1,14 @@
+'''
+Let pn be the nth prime: 2, 3, 5, 7, 11, ..., 
+and let r be the remainder when (pn−1)^n + (pn+1)^n is divided by pn^2.
 
+For example, when n = 3, p3 = 5, and 4^3 + 6^3 = 280 ≡ 5 mod 25.
 
+The least value of n for which the remainder first exceeds 10^9 is 7037.
 
+Find the least value of n for which the remainder first exceeds 10^10.
 
-
-
-
-
-
-
+'''
 
 
 import time
