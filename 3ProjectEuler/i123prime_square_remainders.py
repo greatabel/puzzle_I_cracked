8c9ed57429f355f51eaae6478b515a91b50d0dcc
greatabel <myreceiver2for2github@gmail.com>
2019-03-19 20:41:29 +0800

    traslate problem 123 into Chinese

--- 3ProjectEuler/i123prime_square_remainders.py
+++ 3ProjectEuler/i123prime_square_remainders.py
@@ -8,6 +8,18 @@ The least value of n for which the remainder first exceeds 10^9 is 7037.
 
 Find the least value of n for which the remainder first exceeds 10^10.
 
+#----------------------------#
+翻译：
+欧拉工程/欧拉项目 123: 质数平方的余数们
+
+记pn是第n个素数：2、3、5、7、11……；记r是(pn−1)^n + (pn+1)^n被pn^2除所得的余数。
+
+例如，当n = 3时，p3 = 5，而4^3 + 6^3 = 280 ≡ 5 mod 25。
+
+使余数首次超过109的最小n值是7037。
+
+求使余数首次超过10^10的最小n值。
+
 '''
 
 
