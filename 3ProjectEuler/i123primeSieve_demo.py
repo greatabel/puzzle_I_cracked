ff798ba8d0c4232b486220c30444ba0791c55fb4
greatabel <myreceiver2for2github@gmail.com>
2019-03-20 15:32:08 +0800

    i123primeSieve_demo.py

---
+++ 3ProjectEuler/i123primeSieve_demo.py
@@ -0,0 +1,27 @@
+import math
+import time
+from termcolor import colored
+
+
+def primeSieve(sieveSize):
+    # Returns a list of prime numbers calculated using
+    # the Sieve of Eratosthenes algorithm.
+    sieve = [True] * sieveSize
+
+    sieve[0] = False # zero and one are not prime numbers
+    sieve[1] = False
+
+    # create the sieve
+    for i in range(2, int(math.sqrt(sieveSize)) + 1):
+        pointer = i * 2
+        while pointer < sieveSize:
+            sieve[pointer] = False
+            pointer += i
+
+
+    # compile the list of primes
+    primes = []
+    for i in range(sieveSize):
+        if sieve[i] == True:
+            primes.append(i)
+    return primes
