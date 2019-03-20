cbf04d83d226f3e5653d99c9e52905e55e044200
greatabel <myreceiver2for2github@gmail.com>
2019-03-20 17:20:38 +0800

    solved i124

--- 3ProjectEuler/i124ordered_radicals.py
+++ 3ProjectEuler/i124ordered_radicals.py
@@ -46,13 +46,19 @@ n   rad(n)      n   rad(n)  k
 记E(k)是前n个数排序后的第k个数，例如，E(4) = 8以及E(6) = 9。
 
 对1 ≤ n ≤ 100000按照rad(n)排序后，求E(10000)。
+
+#----------------------------#
+思路：
+老套路了，不可能硬来，我们先把 要求区间的所有素数找出来
+然后创造一个每个数n 和 对应的基数rad(n), 作为一个数对，然后放入列表中
+然后我们就使用筛法：默认每个i 属于n，开始基数都是1
+从每一个素数开始，如果可以被整除，就 rad(i) 乘上这个素数，
+…… 进行下去，因为素数列表的长度应该大大小于 n 的搜索空间
+我们把所有的区间内的素数筛选所有的i 之后，基数就算好了，
+然后就简单了，对列表进行根据基数排序
+然后找到问题需要的第1w个是多少就行了。
+
 '''
-
-
-
-
-
-
 
 
 
@@ -60,9 +66,24 @@ n   rad(n)      n   rad(n)  k
 
 import time
 from termcolor import colored
-
+from i123primeSieve_demo import primeSieve
 
 def main_process():
+    limit = 10**5
+    primes = primeSieve(limit)
+    print(primes)
+    print(colored('-'*20, 'red', attrs=['reverse', 'bold']))
+
+    rads = [[1, i] for i in range(limit + 1)]
+    print(rads)
+
+    for p in primes:
+        print('\n', colored(p, 'red', attrs=['blink', 'bold']))
+        for i in range(p, limit+1, p):
+            # print(i, ' ', end='')
+            rads[i][0] *= p
+            # print('\n', rads)
+    print(sorted(rads)[10000][1])
     print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
