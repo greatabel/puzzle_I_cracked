a91acf2ea5040c7684b7ecdda5ad85797a4943d6
greatabel <myreceiver2for2github@gmail.com>
2019-03-27 22:27:22 +0800

    solved i129

--- 3ProjectEuler/i129repunit_divisibility.py
+++ 3ProjectEuler/i129repunit_divisibility.py
@@ -27,12 +27,36 @@ Find the least value of n for which A(n) first exceeds one-million.
 '''
 
 
+import itertools
+from itertools import count, islice
+from math import sqrt, gcd
 import time
 from termcolor import colored
 
 
+def isprime(n):
+    return n > 1 and all(n%i for i in islice(count(2), int(sqrt(n)-1)))
+
+
+def A(n):
+    if isprime(n) or gcd(n, 10)!= 1:
+        return 0
+    x, k = 1, 1
+    while x != 0:
+        x = (x * 10 + 1) % n
+        k += 1
+        # print('x=', x)
+    return k
+
+
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    # for i in range(2, 10):
+    #     print(i, A(i))
+    Limit = 1000001
+    n = Limit
+    while A(n) < Limit: 
+        n += 2
+    print(colored('mycount=', 'red'), n)
 
 if __name__ == "__main__":
     tic = time.process_time()
