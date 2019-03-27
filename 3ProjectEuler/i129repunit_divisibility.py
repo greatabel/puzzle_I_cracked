477470a619af3542a6c7dc4fadd0393e48e09cfd
greatabel <myreceiver2for2github@gmail.com>
2019-03-27 14:43:28 +0800

    translate i19

--- 3ProjectEuler/i129repunit_divisibility.py
+++ 3ProjectEuler/i129repunit_divisibility.py
@@ -11,6 +11,17 @@ The least value of n for which A(n) first exceeds ten is 17.
 Find the least value of n for which A(n) first exceeds one-million.
 #----------------------------#
 
+欧拉工程129: 循环单位数整除性
+
+只包含数字1的数被称为循环单位数，我们定义R(k)是长为k的循环单位数，例如，R(6) = 111111。
+
+如果n是一个整数，且GCD(n, 10) = 1，可以验证总存在k使得R(k)能够被n整除，并且记A(n)是这些k中最小的一个。
+
+例如，A(7) = 6，而A(41) = 5。
+
+使得A(n)第一次超过十的n是17。
+
+求使得A(n)第一次超过一千万的n
 #----------------------------#
 
 '''
