3ef81af434009d9fb323130987aebfbd74bb89f3
greatabel <myreceiver2for2github@gmail.com>
2019-04-22 17:39:40 +0800

    desc of i131

--- 3ProjectEuler/i131prime_cube_partnership.py
+++ 3ProjectEuler/i131prime_cube_partnership.py
@@ -1,4 +1,17 @@
+'''
+There are some prime values, p, for which there exists a positive integer, n, 
+such that the expression n3 + n2p is a perfect cube.
 
+For example, when p = 19, 8^3 + 8^2×19 = 12^3.
+
+What is perhaps most surprising is that for each prime with this property the value of n is unique, 
+and there are only four such primes below one-hundred.
+
+How many primes below one million have this remarkable property?
+
+#----------------------------#
+
+'''
 
 
 
