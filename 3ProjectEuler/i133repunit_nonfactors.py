3ee0e617bb9f256b267f6154a13e34ce96bc2583
greatabel <myreceiver2for2github@gmail.com>
2019-04-27 22:55:34 +0800

    primeSieve

--- 3ProjectEuler/i133repunit_nonfactors.py
+++ 3ProjectEuler/i133repunit_nonfactors.py
@@ -38,9 +38,33 @@ Find the sum of all the primes below one-hundred thousand that will never be a f
 小于 10^5的质数 是否满足条件，然后统计相关不是这个R(10^n')的质数之和就满足条件了
 '''
 
-
+import math
 import time
 from termcolor import colored
+
+
+def primeSieve(sieveSize):
+    # Returns a list of prime numbers calculated using
+    # the Sieve of Eratosthenes algorithm.
+    sieve = [True] * sieveSize
+
+    sieve[0] = False # zero and one are not prime numbers
+    sieve[1] = False
+
+    # create the sieve
+    for i in range(2, int(math.sqrt(sieveSize)) + 1):
+        pointer = i * 2
+        while pointer < sieveSize:
+            sieve[pointer] = False
+            pointer += i
+
+
+    # compile the list of primes
+    primes = []
+    for i in range(sieveSize):
+        if sieve[i] == True:
+            primes.append(i)
+    return primes
 
 
 def main_process():
