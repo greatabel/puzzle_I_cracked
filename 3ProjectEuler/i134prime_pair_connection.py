9d806ce4ea6835bca283a2ab61628e3b917df03e
greatabel <myreceiver2for2github@gmail.com>
2019-04-29 23:55:32 +0800

    move primeSieve into i134

--- 3ProjectEuler/i134prime_pair_connection.py
+++ 3ProjectEuler/i134prime_pair_connection.py
@@ -36,8 +36,34 @@ Find ∑ S for every pair of consecutive primes with 5 ≤ p1 ≤ 1000000.
 
 '''
 
+
+import math
 import time
 from termcolor import colored
+
+
+def primeSieve(sieveSize):
+    # Returns a list of prime numbers calculated using
+    # the Sieve of Eratosthenes algorithm.
+    sieve = [True] * sieveSize
+
+    sieve[0] = False # zero and one are not prime numbers
+    sieve[1] = False
+
+    # create the sieve
+    for i in range(2, int(math.sqrt(sieveSize)) + 1):
+        pointer = i * 2
+        while pointer < sieveSize:
+            sieve[pointer] = False
+            pointer += i
+
+
+    # compile the list of primes
+    primes = []
+    for i in range(sieveSize):
+        if sieve[i] == True:
+            primes.append(i)
+    return primes
 
 
 def main_process():
