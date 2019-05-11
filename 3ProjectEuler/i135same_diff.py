4691639ce1eeb5c15945112c12c7873a64c1ca40
greatabel <myreceiver2for2github@gmail.com>
2019-05-11 23:46:42 +0800

    translate i135

--- 3ProjectEuler/i135same_diff.py
+++ 3ProjectEuler/i135same_diff.py
@@ -1,8 +1,26 @@
 '''
+Given the positive integers, x, y, and z, 
 
+are consecutive terms of an arithmetic progression, 
+
+the least value of the positive integer, n, for which the equation, x2 − y2 − z2 = n, has exactly two solutions is n = 27:
+
+342 − 272 − 202 = 122 − 92 − 62 = 27
+
+It turns out that n = 1155 is the least value which has exactly ten solutions.
+
+How many values of n less than one million have exactly ten distinct solutions?
 
 #----------------------------#
 
+欧拉工程135: 相同的差
+
+已知正整数x，y，z构成等差数列，使得方程x2 − y2 − z2 = n有两个解的最小正整数为n=27：
+
+342 − 272 − 202 = 122 − 92 − 62 = 27
+使得方程有十个解的最小正整数为n = 1155。
+
+在小于一百万的数中，有多少个n的取值使得方程恰好有十个不同的解？
 
 '''
 
