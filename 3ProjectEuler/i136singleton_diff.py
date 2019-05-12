8812d439fcc7f12b2b0fbff1a0b5544fba488acd
greatabel <myreceiver2for2github@gmail.com>
2019-05-12 22:28:37 +0800

    time 121 seconds to solved i136

--- 3ProjectEuler/i136singleton_diff.py
+++ 3ProjectEuler/i136singleton_diff.py
@@ -15,28 +15,31 @@ How many values of n less than fifty million have exactly one solution?
 
 正整数x，y，z构成等差数列。取正整数n=20，此时方程x2 − y2 − z2 = n只有一个解：
 
-132 − 102 − 72 = 20
+13^2 − 10^2 − 7^2 = 20
 事实上，在小于一百的数中，有25个n的取值使得方程有唯一解。
 
 在小于五千万的数中，有多少个n的取值使得方程有唯一解？
 
 '''
 
-
-
-
-
-
-
-
-
-
 import time
 from termcolor import colored
 
 
+limit = 5 * 10 ** 7
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    # 根据 i135.jpg 分析可知:
+    results = [0] * limit
+    for m in range(1,2 * limit):
+
+        for k in range(m//5 +1, (m+1)//2):
+            n = (m - k) * (5 * k - m)
+            if n >= limit:
+                break
+            results[n] += 1
+    result = results.count(1)
+    print(colored('mycount=', 'red'), result)
+    # mycount= 4989
 
 if __name__ == "__main__":
     tic = time.process_time()
