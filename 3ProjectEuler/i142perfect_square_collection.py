efdd86d90bddc642b4ed756c2287c278463daa68
greatabel <myreceiver2for2github@gmail.com>
2019-05-17 01:51:35 +0800

    fix 142

--- 3ProjectEuler/i142perfect_square_collection.py
+++ 3ProjectEuler/i142perfect_square_collection.py
@@ -22,16 +22,25 @@ such that x + y, x - y, x + z, x - z, y + z, y - z are all perfect squares.
 
 
 
-
-
-
-
 import time
 from termcolor import colored
 
 
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    bound_guess = 1000
+    squares = set([n*n for n in range(1, bound_guess)])
+    for a in range(1, bound_guess):
+        a2 = a*a
+        for b in range(1, a):
+            b2 = b*b
+            for c in range(1, b):
+                c2 = c*c
+                if b2 - c2 in squares and a2 - c2 in squares and a2 - b2 in squares:
+                    if (a2 + b2 + c2) % 2 == 0 and a2 > b2 - c2 and b2 > a2 - c2 and c2 > a2 - b2:
+                       print( (a2 + b2 + c2)/2)
+                       # 1006193
+        
+
 
 if __name__ == "__main__":
     tic = time.process_time()
