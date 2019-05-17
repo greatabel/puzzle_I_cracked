be189754e21e2f41931d179e2fa8f0f75f108931
greatabel <myreceiver2for2github@gmail.com>
2019-05-17 12:33:42 +0800

    fx 143

--- 3ProjectEuler/i143torricelli.py
+++ 3ProjectEuler/i143torricelli.py
@@ -47,24 +47,81 @@ c = 511就是托里拆利三角形的一个例子，此时p + q + r = 784。
 
 对于所有满足p + q + r ≤ 120000的托里拆利三角形，求所有不同的p + q + r的值之和。
 
+#----------------------------#
+
+对这题不感兴趣，本来跳过这道题，本题直接使用：https://randompython.herokuapp.com/blog/4 方案，
+然而发现这个博主其实并没有解决啊：他只解决了小数据量的情况，120000 根本跑不出来（还写博客 这不是坑爹吗！）
+
+只好把 https://www.mathblog.dk/project-euler-143-investigating-the-torricelli-point-of-a-triangle/
+
+方案改成py的
 
 '''
 
 
-
-
-
-
-
-
-
-
 import time
 from termcolor import colored
+from math import gcd
+
+
+limit = 12 * 10 ** 4
+limtSQ = 347
+pairs = []
 
 
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    for u in range(1, limtSQ):
+        for v in range(1, u):
+            if gcd(u, v)!= 1:
+                continue
+            if (u-v) %3 == 0:
+                continue
+
+            a = 2 * u * v + v * v
+            b = u * u - v * v
+            if a+b > limit:
+                break
+            for k in range(1, int(limit /(a + b)) + 1):
+                pairs.append((k*a, k*b))
+                pairs.append((k*b, k*a))
+    pairs.sort()
+    print(pairs[:10])
+    index = [-1] * limit
+    for i in range(0, len(pairs)):
+        if index[pairs[i][0]] == -1:
+            index[pairs[i][0]] = i
+
+    sums = [False] * limit
+    for i in range(0, len(pairs)):
+        a = pairs[i][0]
+        b = pairs[i][1]
+
+        va = []
+        vb = []
+        for j in range(index[a], len(pairs)):
+            mynext = pairs[j]
+            if mynext[0] != a:
+                break
+            va.append(mynext[1])
+
+        for j in range(index[b], len(pairs)):
+            mynext = pairs[j]
+            if mynext[0] != b:
+                break
+            vb.append(mynext[1])
+
+        for ja in range(0, len(va)):
+            if va[ja] in vb and vb.index(va[ja]) != -1 and a + b + va[ja] < limit:
+                sums[a+b+va[ja]] = True
+
+    print('sums=', sums[:10])
+    results = 0
+    for i in range(0, limit):
+        if sums[i]:
+            results += i
+
+    print(colored('mycount=', 'red'), results)
+    # mycount= 30758397
 
 if __name__ == "__main__":
     tic = time.process_time()
@@ -73,3 +130,12 @@ if __name__ == "__main__":
 
     toc = time.process_time()
     print("time=",toc - tic)
+
+
+
+
+
+
+
+
+
