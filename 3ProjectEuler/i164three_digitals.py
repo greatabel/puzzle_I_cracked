cd8fd3526b17ec8b0ab7c93243020602c5390a0c
greatabel <myreceiver2for2github@gmail.com>
2019-08-05 21:40:11 +0800

    desc of 164

--- 3ProjectEuler/i164three_digitals.py
+++ 3ProjectEuler/i164three_digitals.py
@@ -1,10 +1,15 @@
 '''
 
+164: Numbers for which no three consecutive digits have a sum greater than a given value
 
+How many 20 digit numbers n (without any leading zero) exist such 
 
+that no three consecutive digits of n have a sum greater than 9?
 #----------------------------#
 
+欧拉工程164: 没有连续三位数字的和超过给定值的数
 
+有多少个20位数字n（不包括前导0）满足，不存在连续三位数字的和超过9？
 
 '''
 
