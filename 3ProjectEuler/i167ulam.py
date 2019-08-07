207c05293ddb1d221fd025b0d79b0698fa7e379b
greatabel <myreceiver2for2github@gmail.com>
2019-08-07 23:45:42 +0800

    i167ulam.py

---
+++ 3ProjectEuler/i167ulam.py
@@ -0,0 +1,32 @@
+'''
+
+
+
+#----------------------------#
+
+
+
+'''
+
+
+
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
+
+
+
+
+
