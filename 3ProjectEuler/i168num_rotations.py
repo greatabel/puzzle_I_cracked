01ed78c05551070a7047cbcf2dfb3e619538adcb
greatabel <myreceiver2for2github@gmail.com>
2019-08-12 16:36:30 +0800

    desc 168

--- 3ProjectEuler/i168num_rotations.py
+++ 3ProjectEuler/i168num_rotations.py
@@ -1,6 +1,15 @@
 '''
 
+Number Rotations
 
+Problem 168
+Consider the number 142857. We can right-rotate this number by moving the last digit (7) to the front of it, 
+
+giving us 714285.
+It can be verified that 714285=5×142857.
+This demonstrates an unusual property of 142857: it is a divisor of its right-rotation.
+
+Find the last 5 digits of the sum of all integers n, 10 < n < 10100, that have this property.
 
 #----------------------------#
 
