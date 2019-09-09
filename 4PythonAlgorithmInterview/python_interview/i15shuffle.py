ad7f76a60066649ab4ca7fd0109970d6175607f4
greatabel <myreceiver2for2github@gmail.com>
2019-09-09 12:44:59 +0800

    numpy shuffle

--- 4PythonAlgorithmInterview/python_interview/i15shuffle.py
+++ 4PythonAlgorithmInterview/python_interview/i15shuffle.py
@@ -7,7 +7,7 @@
 
 
 '''
-
+from random import shuffle
 import time
 from termcolor import colored
 
@@ -19,6 +19,17 @@ def main_process():
     为了达到这个目的，我们从random模块中导入shuffle()函数。
     '''
     print(colored('mycount=', 'red'), t)
+
+    mylist = list(range(1,10))
+    shuffle(mylist)
+    print(mylist)
+
+    print('numpy 也有shuffle')
+    import numpy as np
+    arr = np.arange(0, 20, 1)
+    print(arr)
+    np.random.shuffle(arr)
+    print(arr)
 
 if __name__ == "__main__":
     tic = time.process_time()
