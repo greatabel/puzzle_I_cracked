48bd0c1b3463bbd2fbf1cafa81a8cb6bef4ace44
greatabel <myreceiver2for2github@gmail.com>
2019-09-11 17:41:14 +0800

    status matrix

--- 4PythonAlgorithmInterview/Interview_feedback/i4zingfront/i1island_area.py
+++ 4PythonAlgorithmInterview/Interview_feedback/i4zingfront/i1island_area.py
@@ -27,16 +27,30 @@ island_matrix = [  [1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1]
          ]
 
+row_len = len(island_matrix)
+col_len = len(island_matrix[0])
 
-def transfer_to_status(original_matrix):
-    row_len = len(original_matrix)
-    col_len = len(original_matrix[0])
-    print(row_len, col_len)
+def transfer_to_status(source_mx):
+    # 第i = 0位置不需要考虑，肯定不符合要求，左边肯定不是0
+    # 最i = len 位置也不需要，右边肯定不符合要求
+    for i in range(1, row_len):
+        for j in range(1, col_len):
+           if source_mx[i][j] == 0:
+            lefts = [source_mx[i][x] for x in range(j)]
+            rights = [source_mx[i][x] for x in range(j+1, col_len)]
+            tops = [source_mx[x][j] for x in range(i)]
+            bottoms = [source_mx[x][j] for x in range(i+1, row_len)]
+            if 1 in lefts and \
+               1 in rights and \
+               1 in tops and \
+               1 in bottoms:
+               source_mx[i][j] = -1
 
-
+    
 def main_process():
+    print(island_matrix, '\n')
     transfer_to_status(island_matrix)
-    print('results')
+    print(island_matrix)
 
 
 if __name__ == "__main__":
