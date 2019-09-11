0c7c6323d4a46cb11fc62f7a31b0bbfc78a0a694
greatabel <myreceiver2for2github@gmail.com>
2019-09-11 18:04:50 +0800

    solved i1island

--- 4PythonAlgorithmInterview/Interview_feedback/i4zingfront/i1island_area.py
+++ 4PythonAlgorithmInterview/Interview_feedback/i4zingfront/i1island_area.py
@@ -27,12 +27,21 @@ island_matrix = [  [1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1]
          ]
 
+# 测试第二个岛的情况
+# island_matrix = [  [1, 1, 1, 1, 1, 1],
+#             [1, 1, 0, 0, 0, 1],
+#             [1, 0, 0, 0, 1, 0],
+#             [1, 1, 0, 1, 1, 1],
+#             [0, 1, 0, 1, 0, 1],
+#             [1, 1, 1, 1, 1, 1]
+#          ]
+
 row_len = len(island_matrix)
 col_len = len(island_matrix[0])
 
 #标示经过计算满足条件的岛屿0
 flag_value = -1
-
+processed_value = 2
 
 def transfer_to_status(source_mx):
     # 第i = 0位置不需要考虑，肯定不符合要求，左边肯定不是0
@@ -50,12 +59,41 @@ def transfer_to_status(source_mx):
                1 in bottoms:
                source_mx[i][j] = flag_value
 
-    
-def main_process():
-    print(island_matrix, '\n')
-    transfer_to_status(island_matrix)
-    print(island_matrix)
+# 因为需要找出所有的满足条件的岛，然后根据岛size排序，找出第2个
+# 所以需要遍历 状态矩阵的每个元素，统计改元素所在的岛的大小，如果
+# 已经统计了该岛的面积，我们就从状态矩阵中踢出
+def get_unique_island_size(status_mx, i, j):
+    if 0 <= i < row_len and 0 <= j < col_len \
+        and status_mx[i][j] == flag_value:
+        status_mx[i][j] = processed_value
+        return 1 + get_unique_island_size(status_mx, i+1, j) + \
+                   get_unique_island_size(status_mx, i-1, j) + \
+                   get_unique_island_size(status_mx, i, j+1) + \
+                   get_unique_island_size(status_mx, i, j-1)
+    # 非环绕岛屿节点为0
+    return 0
 
+
+def get_island_sizes(status_mx):
+    island_sizes = []
+    for i in range(1, row_len):
+        for j in range(1, col_len):
+            if status_mx[i][j] == flag_value:
+                island_size = get_unique_island_size(status_mx, i, j)
+                island_sizes.append(island_size)
+    return island_sizes
+
+
+def main_process():
+    # print(island_matrix, '\n')
+    transfer_to_status(island_matrix)
+    # print(island_matrix)
+    size_list = get_island_sizes(island_matrix)
+    print(size_list)
+    if len(size_list) == 1:
+        print('最大岛面积：', size_list[0])
+    else:
+        print('第2大岛面积：', size_list[1])
 
 if __name__ == "__main__":
     tic = time.process_time()
