31c1da4393b88f34ef09070dae8da5455a03dd33
greatabel <myreceiver2for2github@gmail.com>
2019-09-12 10:54:16 +0800

    i5deeplearning_enginner.py

---
+++ 4PythonAlgorithmInterview/Interview_feedback/i5deeplearning_enginner.py
@@ -0,0 +1,39 @@
+'''
+
+
+numpy
+pandas
+scipy
+
+tensorflow
+keras
+等深度学习模型
+
+常用数据挖掘算法
+
+复杂时序序列预测优先
+
+
+#----------------------------#
+
+
+
+'''
+
+import time
+from termcolor import colored
+
+
+
+
+def main_process():
+    res = []
+    print(res)
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
