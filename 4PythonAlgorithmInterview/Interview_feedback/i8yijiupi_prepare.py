dfe0907d6b5b43f394f0376dfad5d00113c93069
greatabel <myreceiver2for2github@gmail.com>
2019-09-29 00:11:14 +0800

    i8yijiupi_prepare.py

---
+++ 4PythonAlgorithmInterview/Interview_feedback/i8yijiupi_prepare.py
@@ -0,0 +1,35 @@
+'''
+
+
+#----------------------------#
+
+
+
+'''
+
+import time
+from termcolor import colored
+
+
+
+def main_process():
+    t = '''
+
+
+
+
+    '''
+    print(t)
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
+
+
+
+
+
