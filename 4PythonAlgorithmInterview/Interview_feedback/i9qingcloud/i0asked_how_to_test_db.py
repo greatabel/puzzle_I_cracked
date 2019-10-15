37d082eff6163def48c2badb01baeed48397aac9
greatabel <myreceiver2for2github@gmail.com>
2019-10-15 23:45:01 +0800

    i0asked_how_to_test_db.py

---
+++ 4PythonAlgorithmInterview/Interview_feedback/i9qingcloud/i0asked_how_to_test_db.py
@@ -0,0 +1,34 @@
+'''
+
+
+#----------------------------#
+
+
+
+'''
+
+import time
+from termcolor import colored
+
+
+
+def main_process():
+    t = '''
+    1 怎么测试你们python和数据库相关的代码？
+    
+
+    '''
+    print(t)
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
+
+
+
+
+
