851a53da918da2a30287c788b39c72b1ab9b9f3e
greatabel <myreceiver2for2github@gmail.com>
2019-10-18 00:19:52 +0800

    i10a_xinshang.py

--- 4PythonAlgorithmInterview/Interview_feedback/i10a_xinshang.py
+++ 4PythonAlgorithmInterview/Interview_feedback/i10a_xinshang.py
@@ -14,8 +14,10 @@ def findsolution(i, j, k, n):
     思路：
     n % 2 == 1 
         考虑十位以下的情况：
-        当n为奇数，个数上必须有且只能有1个5元在个位，m的个位数上2元数目是固定的： t2single = (n % 10 - 5) // 2
-        除此之外的2元全部都可以换10元：t2 = (k - t2single) * 2 // 10      
+        当n为奇数，
+            当个位大于5时候：个数上必须有且只能有1个5元在个位，m的个位数上2元数目是固定的： t2single = (n % 10 - 5) // 2
+        除此之外的2元全部都可以换10元：t2 = (k - t2single) * 2 // 10     
+
 
         考虑十位以上的情况：
             所有5元可以组成10的个数 total5 =  5 * j // 10
@@ -23,6 +25,11 @@ def findsolution(i, j, k, n):
             1） 手上10元足够自己就足够 i >=  n//10 : 支付 (n//10, 1, t2single)
             2)  需要10元和5元凑十位以上 i <  n//10 ，(n - 10 * i)//5 <= j时：支付(i, (n-10*i)//5, t2single)
             3)  需要10元，5元，2元凑十位以上: i < n//10 and j<(n-10*i)//5 :支付（i,j, (n-10*i-5*j)/2 + t2single)
+        
+        当个位小于5时候：
+            即为1， 只可能为11 = 5 + 2*3
+            或者为3， 只可能13= 5 * 2*4
+            可以从n中分别建去11，13，n变成了n%2 == 0的情况
 
     n % 2 == 0
         考虑十位以下的情况：
@@ -39,8 +46,37 @@ def findsolution(i, j, k, n):
 
     total5= 5 * j // 10
     if n % 2 == 1:
-        if n % 10 <5:
-            print("不能刚好够 n元")
+
+        if n % 10 == 1:
+            n = n - 11
+            t2single = (n % 10) // 2
+            t1 = (k - t2single) * 2 // 10
+            if n // 10 > i + total5+ t1:
+                print("不能刚好凑齐 n元")
+            else:
+                if n // 10 <= i:
+                    print(f"使用{n // 10}张10元，{1}张5元，{t2single+3}张2元，刚好够{n+11}元")
+                else:
+                    if (n - 10 * i) // 5 <= j:
+                        print(f"使用{i}张10元，{(n - 10 * i) // 5 + 1}张5元，{t2single+3}张2元，刚好够{n+11}元")
+                    else:
+                        print(f"使用{i}张10元，{j+1}张5元，{t2single + (n - 10 * i - 5 * j) / 2 + 3}张2元，刚好够{n+11}元")
+                return
+        if n % 10 == 3:
+            n = n - 13
+            t2single = (n % 10) // 2
+            t1 = (k - t2single) * 2 // 10
+            if n // 10 > i + total5+ t1:
+                print("不能刚好凑齐 n元")
+            else:
+                if n // 10 <= i:
+                    print(f"使用{n // 10}张10元，{1}张5元，{t2single+4}张2元，刚好够{n+13}元")
+                else:
+                    if (n - 10 * i) // 5 <= j:
+                        print(f"使用{i}张10元，{(n - 10 * i) // 5 + 1}张5元，{t2single+4}张2元，刚好够{n+13}元")
+                    else:
+                        print(f"使用{i}张10元，{j+1}张5元，{t2single + (n - 10 * i - 5 * j) / 2 + 4}张2元，刚好够{n+13}元")
+            return        
         else:
             t2single = (n % 10 - 5) // 2
             t2 = (k - t2single) * 2 // 10
@@ -78,9 +114,12 @@ if __name__ == "__main__":
         print(i,"个10元 ", j, "个5元", k, "个2元时 ", colored("希望凑总数：", "red"), n, "元 ？")
         tic = time.process_time()
         # testcase: 2张10元， 1张5元，1张2元凑 27元零钱
-        # findsolution(2, 1, 1, 127)
-        # testcase: 10张10元， 5张5元，3张2元凑 127元零钱
 
+        # testcase: 10张10元， 5张5元，3张2元凑 127元零钱
+        # findsolution(2, 1, 3, 11)
+        # findsolution(2, 1, 3, 21)
+        # findsolution(2, 1, 3, 23)
+        # findsolution(2, 1, 3, 33)
         findsolution(i, j, k, n)
         toc = time.process_time()
     print("time=",toc - tic)
